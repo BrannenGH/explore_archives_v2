07c0c12b4e601011b698d0b87011f53819f8d1e0
BrannenGHall <Brannen@HurtadoHall.me>
2017-07-04 00:46:54 -0500

    Add new routes and fix indentaiton

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -12,19 +12,13 @@ import play.twirl.api.Html
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
     val static_title = "Italian Immigrant Stories — The University of Minnesota"
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index.render(static_title))
-  }
-  def document() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.documents.render(static_title))}
-  /**def about() = Action { implicit request:Request[AnyContent] =>
-    Ok(views,html.about.render(""))}
-    */
+    def index() = Action { implicit request: Request[AnyContent] =>
+        Ok(views.html.index.render(static_title))
+    }
+    def document() = Action { implicit request: Request[AnyContent] =>
+        Ok(views.html.documents.render(static_title))
+    }
+    def about() = Action { implicit request:Request[AnyContent] =>
+        Ok(views.html.about.render(static_title))
+    }
 }
